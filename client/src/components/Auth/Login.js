ac98c1a7f5e84364d1d493d50f638aee10bf44ee
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-03-18 13:29:10 -0400

    Settting up OAuth on the client

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,18 +1,20 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+import { withStyles } from '@material-ui/core/styles';
+import { CLIENT_ID } from '../../config';
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  return <GoogleLogin clientId={CLIENT_ID} />;
 };
 
 const styles = {
   root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
   }
 };
 
